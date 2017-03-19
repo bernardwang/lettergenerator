c08461377641da756686fc5d021b152cf5d776f9
bernardwang <bernard@bernard.wang>
2017-03-19 14:19:10 -0500

    promisify

--- src/js/app.js
+++ src/js/app.js
@@ -7,28 +7,31 @@ const styles = [
 	'..%2Fdata%2Ftrainset_diff_no_start_all_labels.nc%2C1970%2B378',
 	'..%2Fdata%2Ftrainset_diff_no_start_all_labels.nc%2C1561%2B872',
 ]
-const maxlen = 100;
+const maxlen = 80;
 
 const getImage = function(apiURL) {
-	$.getJSON('http://anyorigin.com/go?url=' + encodeURIComponent(apiURL) + '&callback=?', (data) => {
-		let el = document.createElement('html');
-		el.innerHTML = data.contents;
-
-		let imgs = $.map($('img', el), (val, i) => {
-			if (i > 5) return val.src;
-			else return;
-		});
-
-		$.map(imgs, (val, i) => {
-			let img = $('<img>');
-			img = $('<img />', { 
-				id: '',
-				src: val,
-				alt: ''
+	let promise = Promise.resolve( 
+		$.getJSON('http://anyorigin.com/go?url=' + encodeURIComponent(apiURL) + '&callback=?', (data) => {
+			let el = document.createElement('html');
+			el.innerHTML = data.contents;
+	
+			let imgs = $.map($('img', el), (val, i) => {
+				if (i > 5) return val.src;
+				else return;
 			});
-			img.appendTo($('#js-main'));
-		});
-	});
+	
+			$.map(imgs, (val, i) => {
+				let img = $('<img>');
+				img = $('<img />', { 
+					id: '',
+					src: val,
+					alt: ''
+				});
+				img.appendTo($('#js-main'));
+			});
+		})
+	);
+	return promise;
 }
 
 const generate = function(e) {
@@ -41,31 +44,37 @@ const generate = function(e) {
 	let url1 = 'http://www.cs.toronto.edu/~graves/handwriting.cgi?text=';
 	let url2 = '&style=' + styles[style] + '&bias=' + bias + '&samples=' + samples;
 
-	// tokenize 
-	// text = text.split(" ");
-	// for (let i = 0; i < text.length; i++) {
-	// 	let apiURL = url1 + text[i] + url2;
-	// 	getImage(apiURL);
-	// }
+	let apiURLs = [];
 
-	// chunking by max len
-	// while (text.length > maxlen) {
-	// 	let subtext = text.slice(0, maxlen);
-	// 	let lastindex = subtext.lastIndexOf(' ');
-	// 	if (lastindex == -1) {
-	// 		console.log('no space');
-	// 		return;
-	// 	}
-	// 	subtext = subtext.slice(0, lastindex);
-	// 	text = text.slice(lastindex);
+	while (text.length > 0) {
+		let apiURL = '';
+		let subtext = '';
 
-	// 	let apiURL = url1 + subtext + url2;
-	// 	getImage(apiURL);
-	// }
+		if (text.length > maxlen) {
+			subtext = text.slice(0, maxlen);
+			let lastindex = subtext.lastIndexOf(' ');
+			if (lastindex == -1) {
+				lastindex = maxlen;
+			}
+			subtext = subtext.slice(0, lastindex).trim();
+			text = text.slice(lastindex).trim();
+		} else {
+			subtext = text;
+			text = '';
+		}
 
+		apiURL = url1 + subtext + url2;
+		apiURLs.push(apiURL);
+	}
 
-	let apiURL = url1 + text + url2;
-	getImage(apiURL);
+	const apiPromises = apiURLs.map((apiURL) => {
+		return getImage(apiURL);
+	});
+
+	return Promise.all(apiPromises)
+		.then(() => {
+			console.log("all done");
+		});
 }
 
 global.app = function () {
